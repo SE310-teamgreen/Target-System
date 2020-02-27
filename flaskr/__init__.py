4f9d06992a51f2c4a8ff63f2d51a9217fa91f96e
dev-lagia <alex.white654@gmail.com>
2020-02-27 16:18:30 -0500

    Added Files

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+	#create and configure the app
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev', #Do Not Keep This when launching. Use a randomly generated key
+		#DATABASE=TODO, please update this
+	)
+	
+	if test_config is None:
+		#load the instance config, if exists, when not testing
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		#load the test config if passed in
+		app.config.from_mapping(test_config)
+
+	#ensure the instance folder exists
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+	#test page that says hello
+	@app.route('/hello')
+	def hello():
+		return 'Hello, World!'
+
+	return app
